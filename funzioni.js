74c0ef4e46212b7381485e8636339d7416959742
andro99 <39365172+andro99@users.noreply.github.com>
2018-10-03 14:07:18 +0200

    Update funzioni.js

--- funzioni.js
+++ funzioni.js
@@ -8,13 +8,50 @@ function calcolo(){
     if(sesso = "m"){
 	var totale = peso/(altezza*altezza);
 	document.getElementById('calcola').innerHTML="BMI ="+totale;
+		if(totale<20.1){
+		document.getElementById('spiegazione').innerHTML="Sottopeso";	
+		}
+		else if(totale<25){
+		document.getElementById('spiegazione').innerHTML="Peso ottimale";		
+		}
+		else if(totale<29.9){
+		document.getElementById('spiegazione').innerHTML="Sovrappeso";	
+		}
+		else if(totale<35){
+		document.getElementById('spiegazione').innerHTML="Obesità di I grado";	
+		}
+		else if(totale<40){
+		document.getElementById('spiegazione').innerHTML="Obesità di II grado";	
+		}
+		else{
+		document.getElementById('spiegazione').innerHTML="Obesità grave";	
+		}
 	}
     else if(sesso="f"){
 	var totale = (peso/(altezza*altezza))*0.9;
 	document.getElementById('calcola').innerHTML="BMI ="+totale;
+		if(totale<18.7){
+		document.getElementById('spiegazione').innerHTML="Sottopeso";	
+		}
+		else if(totale<23.8){
+		document.getElementById('spiegazione').innerHTML="Peso ottimale";	
+		}
+		else if(totale<28.6){
+		document.getElementById('spiegazione').innerHTML="Sovrappeso";	
+		}
+		else if(totale<35){
+		document.getElementById('spiegazione').innerHTML="Obesità di I grado";	
+		}	
+		else if(totale<40){
+		document.getElementById('spiegazione').innerHTML="Obesità di II grado";	
+		}
+		else{
+		document.getElementById('spiegazione').innerHTML="Obesità grave";	
+		}
 	}
 }
 
   function cancella(){
     document.getElementById('calcola').innerHTML=" "; 
+    document.getElementById('spiegazione').innerHTML=" "; 
 }
